14fa1badf9e670390b785b57595743c3ddd11f19
swbenson11 <swbenson11@gmail.com>
2022-07-12 11:27:18 -0600

    added links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import style from './Header.module.css';
 
+// https://reactjs.org/docs/composition-vs-inheritance.html
 const Header = (props) => {
+	// https://dev.to/myogeshchavan97/an-introduction-to-css-modules-in-react-2fkd
 	return <h1 className={style.header}> {props.children}</h1>;
 };
 
